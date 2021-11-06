7baa9cf16698c44b660cec202d2bc6ac859799ed
Artem <68923839+artem-phys@users.noreply.github.com>
2021-11-06 22:11:46 +0300

    fix

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional, List
+import pandas as pd
+import numpy as np
+import math
+
+services_filename = '/content/drive/MyDrive/Colab Notebooks/Объекты-сервисы и потребности.xlsx'
+services = pd.read_excel(services_filename, sheet_name=None)
 
 app = FastAPI()
-
-
-class Place(BaseModel):
-    name: str
-    description: Optional[str] = None
-    coffee: bool
-    wifi: bool
-    food: bool
-    lat: float
-    lng: float
-
-    class Config:
-        orm_mode = True
-
-
-@app.post('/places/')
-async def create_place_view(place: Place):
-    return place
-
 
 @app.get("/")
 def home():
     return {"message": "Hello world"}
 
 
-@app.get("/")
+@app.get("/vc/")
 def recommend_vc(
     user_stage: str, user_market: str
 ):
-    return {"description": user_stage, "2": user_market}
+    return {"Stage": user_stage, "Market": user_market}
