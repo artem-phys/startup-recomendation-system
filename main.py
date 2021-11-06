a0b8fb5202060b70d079abe61d2f02bd76576b23
Artem <68923839+artem-phys@users.noreply.github.com>
2021-11-06 21:53:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from typing import Optional, List
 
 app = FastAPI()
 
+
+class Place(BaseModel):
+    name: str
+    description: Optional[str] = None
+    coffee: bool
+    wifi: bool
+    food: bool
+    lat: float
+    lng: float
+
+    class Config:
+        orm_mode = True
+
+
+@app.post('/places/')
+async def create_place_view(place: Place):
+    return place
+
+
 @app.get("/")
 def home():
-    return {"message":"Hello world"}
+    return {"message": "Hello world"}
